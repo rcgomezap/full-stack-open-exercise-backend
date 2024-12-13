af7345a63157ae017e9ea8c2ed27642b93d69007
Roberto Gomez <rcgomezap@gmail.com>
2024-12-13 18:37:03 -0500

    Added exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3){
+    console.log('You must specify a passsord');
+    process.exit(1)
+}
+
+const pass = process.argv[2]
+
+const url = `mongodb+srv://rcgomeza:${pass}@cluster0.rtp7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person',personSchema)
+
+if (process.argv.length === 3){
+    Person.find({}).then(people => {
+        console.log('Phonebook');
+        people.forEach(person => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+} else {
+
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    }).catch(error => {
+        console.log('Error')
+        mongoose.connection.close()
+    })
+}
