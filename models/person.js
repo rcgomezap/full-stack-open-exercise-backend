a72d8decc4770cafd1809e2d779ac72f9bcb4adf
Roberto Gomez <rcgomezap@gmail.com>
2024-12-16 09:42:30 -0500

    detail

--- models/person.js
+++ models/person.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url).then(result => console.log("Connected to DB"))
+mongoose.connect(url).then(result => console.log("Connected to DB with url", url))
 .catch(er => console.log('Error connecting to db',er.message))
 
 const personSchema = new mongoose.Schema({
