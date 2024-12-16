087f244faef505d7e0a4fcdd72a7ef8cf369f0b3
Roberto Gomez <rcgomezap@gmail.com>
2024-12-16 10:06:35 -0500

    Small fix

--- models/person.js
+++ models/person.js
@@ -12,7 +12,6 @@ const personSchema = new mongoose.Schema({
 
 personSchema.set('toJSON',{
     transform: (document, returnedObject) => {
-        console.log(returnedObject)
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
