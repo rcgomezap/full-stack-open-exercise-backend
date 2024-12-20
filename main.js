f15922590e11b9435b214a377bd023ddec8424e1
Roberto Gomez <rcgomezap@gmail.com>
2024-12-20 09:52:37 -0500

    Updated to exercise 3.18

--- main.js
+++ main.js
@@ -7,68 +7,91 @@ const Persons = require("./models/person")
 const app = express()
 app.use(express.json())
 app.use(cors())
-// app.use(morgan('tiny'))
 morgan.token('type', function (req, res) {return JSON.stringify(req.body) })
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.use(express.static('dist'))
 
-const badRequest = (response,message) => response.status(400).json({error: message}) 
+const badRequest = (response, message) => response.status(400).json({error: message}) 
 const notFound = (response, message) => response.status(404).send(message ? {error: message} : null)
 
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError')
+        return response.status(400).json({error: 'malformatted id'})
 
-app.get("/api/persons", (request,response) => {
+    next(error)
+}
+
+app.get("/api/persons", (request, response) => {
     Persons.find({}).then(persons => {
         response.json(persons)
     })
 })
 
-app.get("/info", (request,response) => {
+app.get("/info", (request, response) => {
     date = new Date()
-    string = `
-    <p>Phonebook has info por ${data.length} people</p>
-    <p>${date.toString()}</p>
-    `
-    response.send(string)
+    Persons.find({})
+    .then(persons => {
+        string = `
+        <p>Phonebook has info por ${persons.length} people</p>
+        <p>${date.toString()}</p>
+        `
+        response.send(string)
+    })
 })
 
-app.get("/api/persons/:id", (request,response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
     Persons.findById(id).then(found => {
         if (found === null)
             return notFound(response)
         response.json(found)})
-    .catch(er => {
-        return notFound(response,'malformatted ID')
+    .catch(er => next(er))
+})
+
+app.post("/api/persons", (request, response) => {
+    body = request.body
+    
+    if (!body.name || !body.number)
+        return badRequest(response, "name or number missing")
+    
+    Persons.find({name: body.name}).then(p => {
+        if (p.length !== 0)
+            return badRequest(response, "name must be unique")
+
+        const person = new Persons({
+            name: body.name,
+            number: body.number
+        })    
+
+        person.save().then(result => response.json(result))
     })
 })
 
-app.post("/api/persons", (request,response) => {
-    body = request.body
-    // console.log(body.name);
-    // console.log(body.number);
-    
-    if (!body.name || !body.number)
-        return badRequest(response,"name or number missing")
-    
-    // if (data.find((p) => p.name === body.name)) // TODO
-    //     return badRequest(response,"name must be unique")
 
-    const person = new Persons({
-        name: body.name,
-        number: body.number
-    })    
-
-    person.save().then(result => response.json(result))
-})
-
-
-app.delete("/api/persons/:id", (request,response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
     Persons.findByIdAndDelete(id).then(data => response.json(data))
+    .catch(er => next(er))
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const id = request.params.id
+
+    const name = request.body.name
+    const number = request.body.number
+
+    const person = {
+        name: name,
+        number: number
+    }
+
+    Persons.findByIdAndUpdate(id, person, { new: true })
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(er => next(er))
 })
 
 const PORT = 3001
+app.use(errorHandler)
 app.listen(PORT, () => console.log("App running"))
 
 
